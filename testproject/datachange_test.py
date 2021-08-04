ba5ed0d95205b4b367f5099d510bda1c16105bca
Szabolcs Petkes <sz.petkes@gmail.com>
2021-08-04 10:07:45 +0200

    latest version

--- testproject/datachange_test.py
+++ testproject/datachange_test.py
@@ -1,49 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import random
+import string
+
+opt = Options()
+opt.headless = True
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
+driver.get('http://localhost:1667/')
+time.sleep(5)
+
 def test_data_change():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from webdriver_manager.chrome import ChromeDriverManager
-    import time
-    import random
-    import string
-
-    opt = Options()
-    opt.headless = True
-
-
-def test_datachange():
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
-
-    driver.get('http://localhost:1667/')
-
     input_data = ["".join([random.choice(string.ascii_lowercase) for _ in range(5)]),
                   f"{random.choice(string.ascii_lowercase)}{random.randint(10, 1000)}@conduit.hu", "Starlord2"]
-    test_data_for_modification = "It's a test sentence."
+    test_data = "Can you read me?"
 
-    def registration_process():
+    def registration():
         driver.find_element_by_xpath("//a[@href='#/register']").click()
-        # time.sleep(2)
+        time.sleep(5)
         for i in range(len(input_data)):
             driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(input_data[i])
             # time.sleep(2)
         driver.find_element_by_tag_name("button").click()
 
-    registration_process()
-    time.sleep(2)
+    registration()
+    time.sleep(5)
 
     driver.find_element_by_xpath("//div[@tabindex='-1']/div/div[4]/div/button").click()
 
-    time.sleep(2)
+    time.sleep(5)
 
     driver.find_element_by_xpath("//a[@href='#/settings']").click()
-    time.sleep(2.0)
-    driver.find_element_by_tag_name("textarea").send_keys(test_data_for_modification)
+    time.sleep(5)
+    driver.find_element_by_tag_name("textarea").send_keys(test_data)
     driver.find_element_by_xpath("//button[@class='btn btn-lg btn-primary pull-xs-right']").click()
-    time.sleep(2.0)
+    time.sleep(5)
     driver.find_element_by_xpath("//button[@class='swal-button swal-button--confirm']").click()
 
-    assert driver.find_element_by_tag_name("textarea").get_attribute("value") == test_data_for_modification
+    assert driver.find_element_by_tag_name("textarea").get_attribute("value") == test_data
     print(f"Text of bio: {driver.find_element_by_tag_name('textarea').get_attribute('value')}")
 
-    time.sleep(3)
+    time.sleep(5)
 
     driver.close()
--- testproject/datadelete_test.py
+++ testproject/datadelete_test.py
@@ -8,12 +8,12 @@ import string
 opt = Options()
 opt.headless = True
 
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
+driver.get('http://localhost:1667/')
+time.sleep(5)
+
 
 def test_data_delete():
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
-    driver.get('http://localhost:1667/')
-    time.sleep(2)
-
     input_data = ["".join([random.choice(string.ascii_lowercase) for _ in range(5)]),
                   f"{random.choice(string.ascii_lowercase)}{random.randint(10, 1000)}@conduit.hu", "Starlord2."]
     data1 = [f"1_title_{random.randint(10, 1000)}", "1_about", "1_text", "1_tag"]
@@ -21,14 +21,16 @@ def test_data_delete():
 
     def registration_process():
         driver.find_element_by_xpath("//a[@href='#/register']").click()
+        time.sleep(5)
         for i in range(len(input_data)):
             driver.find_element_by_xpath(f"//fieldset[{i + 1}]/input").send_keys(input_data[i])
         driver.find_element_by_tag_name("button").click()
+
     registration_process()
-    time.sleep(2)
+    time.sleep(5)
 
     driver.find_element_by_xpath("//div[@tabindex='-1']/div/div[4]/div/button").click()
-    time.sleep(2)
+    time.sleep(5)
 
     def new_article(input_title, input_about, input_text, input_tag):
         driver.find_element_by_xpath("//a[@href='#/editor']").click()
@@ -40,31 +42,31 @@ def test_data_delete():
         driver.find_element_by_xpath("//button[@class='btn btn-lg pull-xs-right btn-primary']").click()
 
     new_article(data1[0], data1[1], data1[2], data1[3])
-    time.sleep(2)
+    time.sleep(5)
 
     print(driver.find_element_by_tag_name("h1").text)
     assert driver.find_element_by_tag_name("h1").text == data1[0]
-    time.sleep(3)
+    time.sleep(5)
     user = driver.find_element_by_xpath("//*[@id='app']/nav/div/ul/li[4]/a")
     user.click()
-    time.sleep(3)
+    time.sleep(5)
     assert len(driver.find_elements_by_xpath("//h1")) == 1
-    time.sleep(3)
+    time.sleep(5)
 
     new_article(data2[0], data2[1], data2[2], data2[3])
-    time.sleep(3)
+    time.sleep(5)
 
     print(driver.find_element_by_tag_name("h1").text)
     assert driver.find_element_by_tag_name("h1").text == data2[0]
     user.click()
-    time.sleep(2)
+    time.sleep(5)
     assert len(driver.find_elements_by_xpath("//h1")) == 2
-    time.sleep(2)
+    time.sleep(5)
     driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div[2]/a/h1").click()
-    time.sleep(2)
+    time.sleep(5)
 
     driver.find_element_by_xpath("//*[@id='app']/div/div[1]/div/div/span/button").click()
-    time.sleep(2)
+    time.sleep(5)
     user.click()
 
     driver.close()
